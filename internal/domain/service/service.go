27330187c22849328296450c0e3d3e26985ac74d
semanurgüçlü <smnrguclu06@gmail.com>
2024-05-01 18:32:49 +0300

    user name validation funch implemented

--- internal/domain/service/service.go
+++ internal/domain/service/service.go
@@ -2,21 +2,22 @@ package domain
 
 import (
 	"strings"
+	"unicode"
 
 	me "github.com/octoposprime/op-be-user/internal/domain/model/entity"
 	mo "github.com/octoposprime/op-be-user/internal/domain/model/object"
 )
 
-// This is the service layer of the domain layer.
+// Service struct for user-related services
 type Service struct {
 }
 
-// NewService creates a new *Service.
+// NewService creates a new instance of Service
 func NewService() *Service {
 	return &Service{}
 }
 
-// ValidateUser validates the user.
+// ValidateUser validates the user based on certain criteria
 func (s *Service) ValidateUser(user *me.User) error {
 	if err := user.Validate(); err != nil {
 		return err
@@ -24,7 +25,7 @@ func (s *Service) ValidateUser(user *me.User) error {
 	return nil
 }
 
-// ValidatePassword validates the user password.
+// ValidatePassword checks the validity of a user's password
 func (s *Service) ValidatePassword(userPassword *me.UserPassword) error {
 	if err := userPassword.Validate(); err != nil {
 		return err
@@ -32,11 +33,12 @@ func (s *Service) ValidatePassword(userPassword *me.UserPassword) error {
 	return nil
 }
 
-// ValidateToken validates the token.
+// ValidateToken checks the validity of a token
 func (s *Service) ValidateToken(token *mo.Token) error {
 	return token.Validate()
 }
 
+// CheckUserNameRules checks the rules for usernames
 func (s *Service) CheckUserNameRules(user *me.User) error {
 	if user.UserName == "" {
 		return mo.ErrorUserUsernameIsEmpty
@@ -50,17 +52,31 @@ func (s *Service) CheckUserNameRules(user *me.User) error {
 	if strings.Contains(user.UserName, " ") {
 		return mo.ErrorUserUsernameIsNotValid
 	}
+	if !isValidUsername(user.UserName) {
+		return mo.ErrorUserUsernameContainsSpecialChar
+	}
 	return nil
 }
 
+func isValidUsername(username string) bool {
+	var hasLetterOrDigit bool
+	for _, ch := range username {
+		if unicode.IsLetter(ch) || unicode.IsDigit(ch) {
+			hasLetterOrDigit = true
+		}
+		if !unicode.IsLetter(ch) && !unicode.IsDigit(ch) && ch != '_' && ch != '.' {
+			return false
+		}
+	}
+	return hasLetterOrDigit // Make sure there is at least one alphanumeric character
+}
+
+// CheckEmailRules validates the email structure
 func (s *Service) CheckEmailRules(user *me.User) error {
 	if user.Email == "" {
 		return mo.ErrorUserEmailIsEmpty
 	}
-	if !strings.Contains(user.Email, ".") {
-		return mo.ErrorUserEmailIsNotValid
-	}
-	if !strings.Contains(user.Email, "@") {
+	if !strings.Contains(user.Email, "@") || !strings.Contains(user.Email, ".") {
 		return mo.ErrorUserEmailIsNotValid
 	}
 	if strings.Contains(user.Email, " ") {
@@ -69,6 +85,7 @@ func (s *Service) CheckEmailRules(user *me.User) error {
 	return nil
 }
 
+// CheckPasswordRules checks the rules for passwords
 func (s *Service) CheckPasswordRules(userPassword *me.UserPassword) error {
 	if userPassword.Password == "" {
 		return mo.ErrorUserPasswordIsEmpty
@@ -85,6 +102,7 @@ func (s *Service) CheckPasswordRules(userPassword *me.UserPassword) error {
 	return nil
 }
 
+// CheckIsAuthenticable checks if a user is authenticable based on their status
 func (s *Service) CheckIsAuthenticable(user *me.User) error {
 	if user.UserStatus == mo.UserStatusINACTIVE {
 		return mo.ErrorUserIsInactive
